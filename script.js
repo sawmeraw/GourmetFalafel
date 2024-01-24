7096c46d302430dbd0c192ebf8ed95cd7fddb216
sawmeraw <pandeysameer96@gmail.com>
2024-01-24 23:32:45 +1030

    added better anims

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const nextButton = document.getElementById("menu-next-page");
 const menuPages = document.querySelectorAll(".menu-page");
 let currentPageIndex = 0;
 
+gsap.registerPlugin(ScrollTrigger);
+
 function firstPageAnim() {
-  var tl = gsap.timeline();
+  const tl = gsap.timeline();
+
   tl.from("#animated-heading", {
     y: "-50",
     opacity: 0,
@@ -24,43 +27,124 @@ function firstPageAnim() {
     duration: 1.4,
     ease: Expo,
   });
-  tl.from(".culinary-items__imgWrapper", {
-    x: "50",
+}
+
+function culinaryAnim() {
+  gsap.from(".culinary-items__imgWrapper.culinary-img-left", {
+    x: "-200",
     opacity: 0,
-    duration: 1.3,
-    ease: Expo.easeInOut,
+    duration: 1.2,
+    ease: Expo,
+    scrollTrigger: {
+      trigger: ".culinary-items__imgWrapper.culinary-img-left",
+      start: "top center",
+      toggleActions: "restart none none none",
+    },
+  });
+
+  gsap.from(".culinary-items__imgWrapper.culinary-img-right", {
+    x: "200",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo,
+    scrollTrigger: {
+      trigger: ".culinary-items__imgWrapper.culinary-img-left",
+      start: "top 80%",
+      toggleActions: "restart none none none",
+    },
+  });
+
+  gsap.from(".culinary-items__content.culinary-content-right", {
+    x: "200",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo,
+    delay: 0.6,
+    scrollTrigger: {
+      trigger: ".culinary-items__imgWrapper.culinary-img-left",
+      start: "top 80%",
+      toggleActions: "restart none none none",
+    },
+  });
+
+  gsap.from(".culinary-items__content.culinary-content-left", {
+    x: "-200",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo,
+    delay: 0.6,
+    scrollTrigger: {
+      trigger: ".culinary-items__imgWrapper.culinary-img-left",
+      start: "top 80%",
+      toggleActions: "restart none none none",
+    },
   });
 }
 
 function openingHoursAnim() {
-  let tl = gsap.timeline();
-  tl.from(".opening-content__hours", {
+  gsap.from(".opening-content__hours", {
     x: "-400",
     opacity: 0,
     duration: 1,
     ease: Expo,
-    scrollTriger: {
-      trigger: ".opening-content__hours",
+    scrollTrigger: {
+      trigger: "#opening",
       start: "top 80%",
-      end: "bottom 20%",
-      scrub: 1,
+      toggleActions: "restart none none none",
     },
   });
 
-  tl.from(".opening-hours__content-dishWrapper", {
-    x: "400",
+  gsap.from(".opening-hours__content-dishWrapper__item.item-top", {
+    y: "-100",
     opacity: 0,
-    duration: 0.9,
+    duration: 1,
     ease: Expo,
+    scrollTrigger: {
+      trigger: "#opening",
+      start: "top 80%",
+      toggleActions: "restart none none none",
+    },
+  });
+  gsap.from(".opening-hours__content-dishWrapper__item.item-bottom", {
+    y: "100",
+    opacity: 0,
+    duration: 1,
+    ease: Expo,
+    scrollTrigger: {
+      trigger: "#opening",
+      start: "top 80%",
+      toggleActions: "restart none none none",
+    },
   });
 }
 
-openingHoursAnim();
+function menuAnim() {
+  gsap.from("#explore-menu", {
+    x: "-200",
+    opacity: 0,
+    duration: 1,
+    ease: Expo,
+    scrollTrigger: {
+      trigger: "#menu",
+      start: "top center",
+      toggleActions: "restart none none none",
+    },
+  });
 
-firstPageAnim();
+  gsap.from("#menu", {
+    x: "200",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+    scrollTrigger: {
+      trigger: "#menu",
+      start: "top center",
+      toggleActions: "restart none none none",
+    },
+  });
+}
 
 function updateButtonText() {
-  // Change button text based on the current page
   nextButton.textContent =
     currentPageIndex === menuPages.length - 1
       ? "< Previous Page"
@@ -76,3 +160,7 @@ nextButton.addEventListener("click", function () {
 });
 
 updateButtonText();
+firstPageAnim();
+menuAnim();
+culinaryAnim();
+document.addEventListener("DOMContentLoaded", openingHoursAnim);
